1986eea48b8495247bd10794f9d73bb6f54fe34c
Catherine Wang <catheri15432077@gmail.com>
2023-07-17 11:14:15 -0700

    Initial Commit

---
+++ The Global Pantry/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  The Global Pantry
+//
+//  Created by CatherineWang on 7/17/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ The Global Pantry/The_Global_PantryApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_Global_PantryApp.swift
+//  The Global Pantry
+//
+//  Created by CatherineWang on 7/17/23.
+//
+
+import SwiftUI
+
+@main
+struct The_Global_PantryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
